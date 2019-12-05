3b44203cb2dfa14ef4fdaec63a2b052ffdb4e126
zrow32 <z.rows32@gmail.com>
2019-12-05 11:00:18 +0300

    Initial Commit

---
+++ BranchDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BranchDemo
+//
+//  Created by Vladimir Zhirov on 12/5/19.
+//  Copyright © 2019 Vladimir Zhirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
